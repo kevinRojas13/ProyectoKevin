de6f6717d3259ecafa86ad17c9f32fd9c4727a82
kevinRojas13 <202000700b@utea.edu.pe>
2023-09-20 23:46:00 -0500

    probando y corrigeindo errores

--- assets/php/registro_usuario_be.php
+++ assets/php/registro_usuario_be.php
@@ -7,10 +7,10 @@ $correo = $_POST['correo'];
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 
-$query = "INSERT INTO T_LOGIN(nombre_completo,correo,usuario,contrasena) // CONSULTA PARA MYSQL
+$query = "INSERT INTO T_LOGIN(nombre_completo,correo,usuario,contrasena)
         VALUES('$nombre_completo','$correo','$usuario','$contrasena')";
 
-$verificar_correo = mysqli_query($conexion,"SELECT*FROM T_LOGIN WHERE correo= '$correo'");
+/*$verificar_correo = mysqli_query($conexion,"SELECT*FROM T_LOGIN WHERE correo= '$correo'");
 if(mysqli_num_rows($verificar_correo)>0){
     echo'
         <script>
@@ -30,6 +30,7 @@ if(mysqli_num_rows($verificar_usuario)>0){
     ';
     exit();
 }
+*/
 $ejecutar = mysqli_query($conexion, $query);
 
 if ($ejecutar) {
